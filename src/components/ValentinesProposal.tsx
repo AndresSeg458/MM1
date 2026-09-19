629fdaebf231ed7152f91654ea11229aec52074b
Andres-Local <segoviarosero@unicauca.edu.co>
2026-09-19 11:27:48 -0500

    Ultimos ajustes

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -175,8 +175,8 @@ export default function ValentinesProposal() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            Thank you for accepting, I love you! 💕
-            <p className="text-sm mt-4">For more information, write me!!! 💌</p>
+            Te quiero muchísimo 💕
+            <p className="text-sm mt-4">para más información, escribeme!!! 💌</p>
             <Image
               src="/hamster_jumping.gif"
               alt="Hamster Feliz"
